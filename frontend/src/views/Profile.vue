337f608f52ef469f0be8f4cf89217358f95b8ada
mkblbj <mkblbj@gmail.com>
2026-04-23 11:06:12 +0900

    fix: always show settings entry in profile

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -69,10 +69,7 @@
 						</div>
 
 						<!-- Settings -->
-						<div
-							class="flex flex-col gap-5 my-4 w-full"
-							v-if="allowPushNotifications"
-						>
+						<div class="flex flex-col gap-5 my-4 w-full">
 							<div class="flex flex-col bg-white rounded">
 								<router-link
 									:to="{ name: 'Settings' }"
@@ -142,7 +139,7 @@
 </template>
 
 <script setup>
-import { computed, inject, ref, onMounted, onBeforeUnmount } from "vue"
+import { inject, ref, onMounted, onBeforeUnmount } from "vue"
 import { useRouter } from "vue-router"
 import { IonModal, IonPage, IonContent } from "@ionic/vue"
 import { FeatherIcon, createDocumentResource, createResource } from "frappe-ui"
@@ -152,8 +149,6 @@ import { formatCurrency } from "@/utils/formatters"
 
 import ProfileInfoModal from "@/components/ProfileInfoModal.vue"
 import PasskeyManager from "@/components/PasskeyManager.vue"
-
-import { arePushNotificationsEnabled } from "@/data/notifications"
 
 const DOCTYPE = "Employee"
 
@@ -221,12 +216,6 @@ const profileLinks = [
 
 const isInfoModalOpen = ref(false)
 const selectedItem = ref(null)
-
-const allowPushNotifications = computed(
-	() =>
-		window.frappe?.boot.push_relay_server_url &&
-		arePushNotificationsEnabled.data
-)
 
 const openInfoModal = async (request) => {
 	selectedItem.value = request
